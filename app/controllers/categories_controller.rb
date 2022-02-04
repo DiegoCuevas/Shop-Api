13880614b9044b62fd83ef1dc602afb5c64f9649
Diego Cuevas <diegocuevas579@gmail.com>
2022-02-04 13:48:22 -0500

    ADD: crud for category controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,48 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :update, :destroy]
+
+  def index
+    @category = Category.all
+    render json: @category
+  end
+
+  def show
+    render json: @category
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      render json: @category, status: :created
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @category.update(category_params)
+      render json: @category
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+  end
+
+  private
+
+  def set_category
+    @category = Category.search(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+
+  end    
 end
