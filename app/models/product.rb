6dac024b37fee39346168e7604d2d05f00a740c3
Diego Cuevas <diegocuevas579@gmail.com>
2022-02-10 23:12:43 -0500

    ADD: table name and functions to filter in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ApplicationRecord
+  self.table_name = "product"
+
   belongs_to :category
+
+  def self.search(search)
+    all.where('name LIKE ?', "%#{search}%")
+  end
+
+  def self.order_by_category(category_id)
+    all.where(category_id: category_id)
+  end
+
 end
