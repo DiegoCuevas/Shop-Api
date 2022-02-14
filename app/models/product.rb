cd6be902503d86f4b12cf657922633ffb7b3a57f
Diego Cuevas <diegocuevas579@gmail.com>
2022-02-14 12:04:29 -0500

    Fix: search for category name

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,13 @@
 class Product < ApplicationRecord
   self.table_name = "product"
+  belongs_to :category, foreign_key: "category"
 
   def self.search(search)
-    all.where('name LIKE ?', "%#{search}%")
+    all
+      .joins(:category)
+      .where("lower(product.name) LIKE ?", "%#{search.downcase}%")
+      .or(Product.joins(:category)
+      .where("lower(category.name) LIKE ? ", "%#{search.downcase}%"))
   end
 
   def self.order_by_category(category_id)
